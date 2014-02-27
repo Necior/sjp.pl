e2352c74e2692fe42fc92a99759623245deb08e1
Adrian Sadłocha <adrian.adek@gmail.com>
2014-02-27 23:11:58 +0100

    Changed exit() to sys.exit()

--- sjp.py
+++ sjp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import urllib.request # to download HTML source
-import sys # to access CLI arguments
+import sys # to access CLI arguments and to use exit codes
 from bs4 import BeautifulSoup # to parse HTML source
 
 version = 0.01
@@ -26,7 +26,7 @@ def getDefinition(word):
 	# checks if definition is in dictionary:
 	if soup.find_all('span', style="color: #e00;"):
 	  print("[Error] \"" + word + "\" not found")
-	  exit()
+	  sys.exit()
 
 	# definition is in dictionary, continue:
 	ex = soup.find_all('p', style="margin-top: .5em; font-size: medium; max-width: 30em; ")[0]
@@ -36,15 +36,15 @@ def main():
 	if len(sys.argv) > 1:
 		if sys.argv[1] in ["-h", "--help", "/?"]:
 			printUsageInfo()
-			exit()
+			sys.exit()
 		elif sys.argv[1] in ["-v", "--version"]:
 			printVersionInfo()
-			exit()
+			sys.exit()
 		else:
 			print('\n'.join(getDefinition(sys.argv[1])))
 	else:
 		printUsageInfo()
-		exit()
+		sys.exit()
 	
 if __name__ == "__main__":
 	main()
