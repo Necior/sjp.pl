62ebfd82dd0c6b2419bab2fa12540879d96927be
Adrian Sadłocha <adrian@sadlocha.eu>
2015-04-12 23:38:27 +0200

    Make Python code PEP 8 compliant

--- sjp.py
+++ sjp.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import urllib.request # to download HTML source
-import sys # to access CLI arguments and to use exit codes
-from bs4 import BeautifulSoup # to parse HTML source
+import urllib.request  # to download HTML source
+import sys  # to access CLI arguments and to use exit codes
+from bs4 import BeautifulSoup  # to parse HTML source
 
 version = 0.01
+
 
 def printUsageInfo():
     helpMsg = """Usage:
@@ -13,9 +14,11 @@ def printUsageInfo():
     sjp.py (-v | --version)"""
     print(helpMsg)
 
+
 def printVersionInfo():
     versionMsg = "sjp.py " + str(version)
     print(versionMsg)
+
 
 def getDefinition(word):
     url = 'http://sjp.pl/' + urllib.parse.quote(word)
@@ -25,12 +28,17 @@ def getDefinition(word):
 
     # checks if definition is in dictionary:
     if soup.find_all('span', style="color: #e00;"):
-      print("[Error] \"" + word + "\" not found")
-      sys.exit(1)
+        print("[Error] \"" + word + "\" not found")
+        sys.exit(1)
 
     # definition is in dictionary, continue:
-    ex = soup.find_all('p', style="margin-top: .5em; font-size: medium; max-width: 32em; ")[0]
-    return ex.contents[0::2] # returns a list of lines of definition
+    ex = soup.find_all('p',
+                       style="margin-top: .5em; "
+                             "font-size: medium; "
+                             "max-width: 32em; ")
+    ex = ex[0]
+    return ex.contents[0::2]  # returns a list of lines of definition
+
 
 def main():
     if len(sys.argv) > 1:
@@ -45,6 +53,6 @@ def main():
     else:
         printUsageInfo()
         sys.exit()
-    
+
 if __name__ == "__main__":
     main()
